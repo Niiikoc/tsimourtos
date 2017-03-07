7b380fe96889c1c38e881d593afe3e277b01a140
Niiikoc <nicktsimo7@hotmail.com>
2017-03-08 01:08:13 +0200

    nick tsimourtos random nums

---
+++ random number/random number/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace random_number
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.textBox1.Location = new System.Drawing.Point(169, 126);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 70);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.textBox2.Location = new System.Drawing.Point(275, 126);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 70);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.textBox3.Location = new System.Drawing.Point(385, 126);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 70);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.textBox4.Location = new System.Drawing.Point(491, 126);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 70);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.textBox5.Location = new System.Drawing.Point(609, 126);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 70);
+            this.textBox5.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1022, 537);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ random number/random number/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace random_number
+{
+    public partial class Form1 : Form
+    {
+        class number
+        {
+            public void _arithmos(int x) { num = x; }
+            public void _arithmos2(int y) { num2 = y; }
+            public void _arithmos3(int z) { num3 = z; }
+            public void _arithmos4(int w) { num4 = w; }
+            public void _arithmos5(int f) { num5 = f; }
+            public int arithmos()
+            {
+                Random rnd = new Random();
+                int val = rnd.Next(1, 50);
+                num = val;
+                return num;
+            }
+            public int arithmos2()
+            {
+                Random rnd2 = new Random();
+                int val2 = rnd2.Next(1, 100) + 3;
+                num2 = val2;
+                return num2;
+            }
+            public int arithmos3()
+            {
+                Random rnd3 = new Random();
+                int val3 = rnd3.Next(100) + 2;
+                num3 = val3;
+                return num3;
+            }
+            public int arithmos4()
+            {
+                Random rnd4 = new Random();
+                int val4 = rnd4.Next(100) + 7;
+                num4 = val4;
+                return num4;
+            }
+            public int arithmos5()
+            {
+                Random rnd5 = new Random();
+                int val5 = rnd5.Next(1, 100) ;
+                num5 = val5;
+                return num5;
+            }
+            private int num, num2, num3, num4, num5;
+        }
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            
+        }
+        int count = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            number numb = new number();
+            textBox1.Text = (numb.arithmos().ToString());
+            textBox2.Text = (numb.arithmos2().ToString());
+            textBox3.Text = (numb.arithmos3().ToString());
+            textBox4.Text = (numb.arithmos4().ToString());
+            textBox5.Text = (numb.arithmos5().ToString());
+            textBox5.Text = (numb.arithmos5().ToString());
+            ++count;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+timer1.Start();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void mouseClick(object sender, MouseEventArgs e)
+        {
+            timer1.Stop();
+
+            int a, b, c, d, f;
+            int result;
+            a = int.Parse(textBox1.Text);
+            b = int.Parse(textBox2.Text);
+            c = int.Parse(textBox3.Text);
+            d = int.Parse(textBox4.Text);
+            f = int.Parse(textBox5.Text);
+            result = a + b + c + d + f;
+            MessageBox.Show((result.ToString()),"Το αποτέλεσμα ειναι: ");
+
+        }
+    }
+}
